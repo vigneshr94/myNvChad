493835d51ae7e33cbbc108589affb73000f217d3
R Vigneswar <vigneshr.1894@gmail.com>
2024-03-15 02:44:11 +0530

    added my custom dashboard

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -2,6 +2,29 @@ local M = {}
 
 M.ui = {
   theme = "catppuccin",
+  nvdash = {
+    load_on_startup = true,
+    header = {
+      "                                   ",
+      "                                   ",
+      "                                   ",
+      "          HI, VIGNESH !!!          ",
+      "        Happy Tinkering  ;)        ",
+      "                                   ",
+      "                                   ",
+      "                                   ",
+      "                                   ",
+    },
+
+    buttons = {
+      { "  Find File", "Spc f f", "Telescope find_files" },
+      { "󰈚  Recent Files", "Spc f o", "Telescope oldfiles" },
+      { "󰈭  Find Word", "Spc f w", "Telescope live_grep" },
+      { "  Bookmarks", "Spc m a", "Telescope marks" },
+      { "  Themes", "Spc t h", "Telescope themes" },
+      { "  Mappings", "Spc c h", "NvCheatsheet" },
+    },
+  },
 }
 
 return M
