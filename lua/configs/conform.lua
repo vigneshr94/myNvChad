71fb2499694c0c7f5caea7af270c7e52966f0371
R Vigneswar <vigneshr.1894@gmail.com>
2024-04-10 18:31:02 +0530

    added formaters to each language

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,9 +1,17 @@
 local options = {
-  lsp_fallback = true,
+	lsp_fallback = true,
 
-  formatters_by_ft = {
-    lua = { "stylua", "black", "isort", "prettier" },
-  },
+	formatters_by_ft = {
+		lua = { "stylua" },
+		python = { "black", "isort" },
+		javascript = { "prettier" },
+		yaml = { "prettier" },
+		typescript = { "prettier" },
+		json = { "prettier" },
+		html = { "prettier" },
+		markdown = { "prettier" },
+		css = { "prettier" },
+	},
 }
 
 require("conform").setup(options)
